00ff09d486f434f0d54cddcb2da71bf22a6d826c
Eric Mwenda <ericmwenda5@gmail.com>
2017-04-11 22:12:13 +0300

    adding stock

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -1,5 +1,12 @@
 module ProductsHelper
   def print_price(price)
-  number_to_currency price
-end
+    number_to_currency price
+  end
+  def print_stock(stock)
+    if stock == 0
+      content_tag(:span, "Out of stock", class: "out_stock")
+    else
+      content_tag(:span, "In stock (#{stock})", class: "in_stock")
+    end
+  end
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,7 @@ class Product < ActiveRecord::Base
   def price=(input)
   input.delete!("$")
   super
-end
-validates_numericality_of :price
+  end
+  validates_numericality_of :price
+  validates_numericality_of :stock
 end
