02389e2eedb7a2fed8b5eda1c8423db0333057e8
Eric Mwenda <ericmwenda5@gmail.com>
2017-04-29 10:58:53 +0300

    'designing Orders Compleate

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,22 +1,6 @@
 class OrderItemsController < ApplicationController
+  before_action :set_order_item, only: [:show, :edit, :destroy]
   before_action :load_order, only: [:create]
-  before_action :set_order_item, only: [:show, :edit, :update, :destroy]
-  # GET /order_items
-  # GET /order_items.json
-#  def index
-    #@order_items = OrderItem.all
-  #end
-
-  # GET /order_items/1
-  # GET /order_items/1.json
-  #def show
-  #end
-
-  # GET /order_items/new
-#def new
-  #  @order_item = OrderItem.new
-#  end
-
   # GET /order_items/1/edit
   def edit
   end
@@ -24,10 +8,13 @@ class OrderItemsController < ApplicationController
   # POST /order_items
   # POST /order_items.json
   def create
-    @order_item = OrderItem.new(product_id: params[:product_id], order_id: @order.id)
+    @order_item = @order.order_items.find_or_initialize_by(product_id: params[:product_id], order_id: @order.id)
+    #@order_item = OrderItem.new(product_id: params[:product_id], order_id: @order.id, quantity: 1)
+  #  @order_item.quantity += 1
+
     respond_to do |format|
       if @order_item.save
-        format.html { redirect_to @order, notice: 'Successfully added product to cart.' }
+        format.html { redirect_to @order, notice: 'Order item was successfully created.' }
         format.json { render :show, status: :created, location: @order_item }
       else
         format.html { render :new }
@@ -40,8 +27,12 @@ class OrderItemsController < ApplicationController
   # PATCH/PUT /order_items/1.json
   def update
     respond_to do |format|
-      if @order_item.update(order_item_params)
-        format.html { redirect_to @order_item, notice: 'Order item was successfully updated.' }
+      @order_item = OrderItem.find(params[:id])
+      if params[:order_item][:quantity].to_i == 0
+        @order_item.destroy
+        format.html { redirect_to orders_path, notice: 'Order item was successfully destroyed.' }
+      elsif @order_item.update(order_item_params)
+        format.html { redirect_to orders_path, notice: 'Order item was successfully updated.' }
         format.json { render :show, status: :ok, location: @order_item }
       else
         format.html { render :edit }
@@ -55,7 +46,7 @@ class OrderItemsController < ApplicationController
   def destroy
     @order_item.destroy
     respond_to do |format|
-      format.html { redirect_to order_items_url, notice: 'Order item was successfully destroyed.' }
+      format.html { redirect_to orders_url, notice: 'Order item was successfully destroyed.' }
       format.json { head :no_content }
     end
   end
@@ -66,16 +57,16 @@ class OrderItemsController < ApplicationController
       @order_item = OrderItem.find(params[:id])
     end
 
-    def load_order
-        @order = Order.find_or_initialize_by_id(session[:order_id], status: "unsubmitted")
-      if @order.new_record?
-      @order.save!
-      session[:order_id] = @order.id
-    end
-   end
-
     # Never trust parameters from the scary internet, only allow the white list through.
     def order_item_params
       params.require(:order_item).permit(:product_id, :order_id, :quantity)
+    end
+    #session to find session order id we pass at "Add to cart"
+    def load_order
+      @order = Order.find_or_initialize_by(id: session[:order_id], status: "unsubmitted", user_id: session[:user_id])
+      if @order.new_record?
+        @order.save!
+        session[:order_id] = @order.id
+      end
     end
 end
