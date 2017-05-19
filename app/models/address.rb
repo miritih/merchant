c83f9b46c401721815849fcba6ac260cf5da0e5a
Eric Mwenda <ericmwenda5@gmail.com>
2017-05-18 19:00:00 -0600

    Implemented address selection on orders

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,12 @@
 class Address < ActiveRecord::Base
+	belongs_to :user
+	has_many :orders
+	validates :line1, :city, :state, :zip, presence: true
+	validates_numericality_of :zip
+	validates :line1, length: { minimum: 5 }
+	validates :state, length: { is: 2 }
+
+	def to_s
+		[line1, city, state, zip].compact.join(', ')
+	end
 end
